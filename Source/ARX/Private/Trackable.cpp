e67dc0e6fae660c63ed9e179208002b85b2913b3
Anssi <anssi.grohn@karelia.fi>
2018-09-01 21:01:10 +0300

    working implementation

--- Source/ARX/Private/Trackable.cpp
+++ Source/ARX/Private/Trackable.cpp
@@ -12,35 +12,48 @@ Trackable::~Trackable()
 {
   
 }
+
 void
 Trackable::MatrixToTransform( ARdouble transformMatrix[3][4], FTransform & transform )
 {
 
     ARdouble transInverted[3][4];
     ARdouble quaternion[4];
-    ARdouble position[3];
-
     arUtilMatInv(transformMatrix, transInverted);
-    arUtilMat2QuatPos(transInverted, quaternion, position);
-    //float aspectRatioFix = (float)cameraSize.X / (float)cameraSize.Y;
-    FVector pos = FVector(transformMatrix[2][3], transformMatrix[0][3], -transformMatrix[1][3]);
-    //pos /= aspectRatioFix;
-    //pos = FVector(markersNFT[0].trans[0][3]/1.777, markersNFT[0].trans[1][3]/1.777, -markersNFT[0].trans[2][3]/1.777);
+    
+    
+    FVector pos = FVector(transformMatrix[2][3], -transformMatrix[0][3], transformMatrix[1][3]);
+    
+    auto & tmp = transInverted;
+  
+   
+    // Extract Head/Yaw, Pitch and Roll from matrix. 
+    // UE4 FMatrix rotation conversion seem to compute it differently it some fashion, 
+    // and do not provide correct result.
+    float h = atan2f(-tmp[2][0], tmp[2][2]);
+    float p = asinf( tmp[2][1]);
+    float r = 0;
+    if ( FMath::IsNearlyZero(cosf(p),  0.001f) )
+    {
+        r = atan2f(tmp[1][0], tmp[0][0]);
+    }
+    else r = atan2f( -tmp[0][1], tmp[1][1]);
+    
+   
+   
+    //UE_LOG(LogTemp, Warning, TEXT("Matrix rotator: %s"),*matT.Rotator().ToString());
+    
     //Construct proper rotation for UE coordinates
-    FRotator raw = FQuat(quaternion[0], quaternion[1], quaternion[2], quaternion[3]).Rotator();
     FRotator rot;
-    rot.Yaw = -raw.Pitch;
-    rot.Pitch = 180 - raw.Roll;
-    rot.Roll = raw.Yaw;
-
-    //rot = raw;
-    //TODO
-    // Need offset for target (from left down corner to center, for example)
-
-    //UE_LOG(LogTemp, Log, TEXT("Pattern found, errorlevel is  %f"), errorLevel);    
-
-
-    transform = FTransform( rot, pos, FVector(1,1,1));
+    rot.Yaw = FMath::RadiansToDegrees(h);
+    rot.Pitch = FMath::RadiansToDegrees(p);
+    // ARToolkitX provides matrix in right-handed coordinate system, we need to flip roll value. 
+    rot.Roll = -FMath::RadiansToDegrees(r);
+    
+    //UE_LOG(LogTemp, Warning, TEXT("Final rotator: %s"),*rot.ToString());
+    transform = GetOffset();
+    // Append offset, allows relative positioning of matching actor.
+    transform.Accumulate(FTransform( rot, pos, FVector(1,1,1)));
 
 }
 
@@ -74,4 +87,10 @@ FTransform Trackable::ApplyParentCameraToTransform(const FTransform & InTransfor
     UE_LOG( LogTemp, Warning, TEXT("ApplyParentCameraToTransform: Parent camera actor missing, cannot do anything."));
     return InTransform;
   }
+}
+
+
+FTransform Trackable::GetOffset() const
+{
+  return FTransform::Identity;
 }
